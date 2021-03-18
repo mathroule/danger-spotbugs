2a735a83d3497df83ef6c75e5b4de23d63393bc5
Mathieu Rul <mathroule@gmail.com>
2021-03-18 18:45:46 +0100

    Fix rubocop offense

--- spec/spotbugs_spec.rb
+++ spec/spotbugs_spec.rb
@@ -1,4 +1,6 @@
-require File.expand_path("../spec_helper", __FILE__)
+# frozen_string_literal: true
+
+require File.expand_path("spec_helper", __dir__)
 
 module Danger
   describe Danger::DangerSpotBugs do
@@ -6,23 +8,11 @@ module Danger
       expect(Danger::DangerSpotBugs.new(nil)).to be_a Danger::Plugin
     end
 
-    #
-    # You should test your custom attributes and methods here
-    #
     describe "with Dangerfile" do
       before do
         @dangerfile = testing_dangerfile
-        @my_plugin = @dangerfile.spotbugs
-
-        # mock the PR data
-        # you can then use this, eg. github.pr_author, later in the spec
-        json = File.read(File.dirname(__FILE__) + '/support/fixtures/github_pr.json') # example json: `curl https://api.github.com/repos/danger/danger-plugin-template/pulls/18 > github_pr.json`
-        allow(@my_plugin.github).to receive(:pr_json).and_return(json)
+        @spotbugs = @dangerfile.spotbugs
       end
-
-      # Some examples for writing tests
-      # You should replace these with your own.
-
     end
   end
 end
